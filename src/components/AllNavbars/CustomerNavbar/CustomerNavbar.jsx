c36ad3d92f06cb969d2e0ebdd2ef697a8cc4d9f3
Gabriel Lima <102686294+GabrielLima07@users.noreply.github.com>
2024-12-07 19:03:24 -0300

    style: modify spacement and cursor

--- src/components/AllNavbars/CustomerNavbar/CustomerNavbar.jsx
+++ src/components/AllNavbars/CustomerNavbar/CustomerNavbar.jsx
@@ -101,7 +101,7 @@ const CustomerNavbar = () => {
           </div>
           <div className="flex flex-1 items-center justify-center sm:items-stretch sm:justify-start">
             <div className="flex flex-shrink-0 items-center">
-              <h1 className='text-white pb-1 text-lg cursor-pointer' onClick={() => navigate("/clienteHome")}>WayClient</h1>
+              <h1 className='text-white pb-1 text-3xl cursor-pointer' onClick={() => navigate("/clienteHome")}>WayClient</h1>
             </div>
             <div className="hidden sm:ml-6 sm:block">
               <div className="flex space-x-4 text-white mt-1">
